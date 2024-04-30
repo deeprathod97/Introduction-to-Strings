d9298621e783cc8b1cbd25d09d7a078c45506bda
deeprathod97 <deeprathod9797@gmail.com>
2024-04-30 12:12:21 +0530

    Introduction to Strings

---
+++ script.js
@@ -0,0 +1,27 @@
+let name = "Deep Rathod"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+let friend = 'Kunal'
+console.log(friend)
+// let myfriend = "kunal' // Dont do this 
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "Pramod"
+let boy2 = "nikhil"
+
+//Nikhil is a friend of pramod 
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+// Escape Sequence Characters
+/*let fruit = 'Bana\'na'
+console.log(fruit)
+console.log(fruit.length)*/
+
+let fruit = "Bana\"na"
+console.log(fruit)
+console.log(fruit.length)
